406efa5b5701319aeaec8e29f53db21dd0f4c696
Konstantin Chernov <chkv@roscluster.ru>
2023-09-21 14:40:29 +0300

    Добавлено расписание запуска периодической задачи по очистке просроченных токенов

--- config/celery_app.py
+++ config/celery_app.py
@@ -2,8 +2,8 @@ import os
 import logging
 
 from celery import Celery
+from celery.schedules import crontab
 import django
-
 
 logger = logging.getLogger(__name__)
 
@@ -13,6 +13,20 @@ django.setup()
 
 app = Celery('celery_app')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.conf.beat_schedule = {
+    # Executes every day 3:00 a.m.
+    'every_day_delete_expired_tokens_from_db': {
+        'task': 'app.authorization.tasks.delete_expired_tokens_from_db',
+        'schedule': crontab(hour=3, minute=0),
+        # 'args': (),
+    },
+    # 'test-every-5-seconds': {
+    #     'task': 'celery_app.test_task',
+    #     'schedule': 5.0,
+    #     'args': (16, 16)
+    # },
+}
 
 
 app.autodiscover_tasks()
